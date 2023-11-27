5f20038a3449395a50d80dc7df759ad0eb70572c
krupali-patel23 <123234608+krupali-patel23@users.noreply.github.com>
2023-11-27 12:29:55 +0530

    React JS

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import SignInSide from './Component/SignInSide';
+// import Demo1 from './Component/Demo1';
+// import Render from './Component/Render';
+function App() {
+  return (
+    <div className="App">
+     {/* <Demo1 />  */}
+     <SignInSide />
+      
+    </div>
+  );
+}
+
+export default App;
+
---
+++ my-app/src/Component/Demo1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo1 = () =>{
+
+    return(
+        <div>
+            <h4>Hello World...</h4>
+        </div>
+    )
+}
+export default Demo1;
