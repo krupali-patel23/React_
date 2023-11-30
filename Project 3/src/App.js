a57aa2cdc3ede70f88a95102251190a14c1c2c4f
krupali-patel23 <123234608+krupali-patel23@users.noreply.github.com>
2023-11-30 11:44:20 +0530

    pr3
    pr3

---
+++ Project 3/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header';
+// import NavBar from './Component/Navbar';
+import Blog from './Component/Blog';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Blog />
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
