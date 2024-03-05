83cc59553210611dd609976fdf9467e598e0f5f5
Krupali <krupalizalavadiya23@gmail.com>
2024-03-05 12:13:28 +0530

    Api
    calling

---
+++ Api/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Apicomponent from './Apicomponent';
+
+function App() {
+  return (
+    <div className="App">
+        <Apicomponent/>
+    </div>
+  );
+}
+
+export default App;
